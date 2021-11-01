af621960f8bf96abb1f6068a3424069dff42654c
Paul Krisman <paul@relovephotos.com>
2021-11-01 18:21:52 +0000

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const forecast = require('./utils/forecast')
 
 const app = express()
 
+const port = process.env.PORT || 3000
+
 //define paths and static directory
 app.use(express.static(path.join(__dirname, '../public')))
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -101,6 +103,6 @@ app.get('*', (req,res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up at port 3000')
+app.listen(port, () => {
+    console.log('Server is up at port ' + port)
 })
